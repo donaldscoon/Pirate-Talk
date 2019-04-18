c725cb9bfef0a845a53a6150be5cc02a301f5fa1
Donald <donaldscoon@gmail.com>
2019-04-18 14:31:52 -0700

    pirate talk done

--- pirate_talk.py
+++ pirate_talk.py
@@ -9,6 +9,8 @@ import argparse
 import sys
 import os
 import re
+import random
+import time
 
 # --------------------------------------------------
 def get_args():
@@ -17,16 +19,14 @@ def get_args():
         description='Argparse Python script',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    # parser.add_argument(
-    #     'Name  ', metavar='str', help='What shall we call you, ya land lubber')
-
-    # parser.add_argument(
-    #     '-k',
-    #     '--overlap',
-    #     help='Number of codons to over lap at begining and end of sequence',
-    #     metavar='int',
-    #     type=int,
-    #     default=3)
+    parser.add_argument(
+        '-r',
+        '--rounds',
+        help='Number of rounds in the insult competition',
+        metavar='int',
+        type=int,
+        default='',
+        required=True)
 
     return parser.parse_args()
 
@@ -43,36 +43,60 @@ def die(msg='Something bad happened'):
     warn(msg)
     sys.exit(1)
 
-#---------------------------------------------------
-def tim_says():
-
-
-
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
     args = get_args()
+    rounds = args.rounds
 
     word1 = """
     brainless stupid pin-headed blathering
     donkey-eared bleating idiotic
     cricket-sized pig-breathed
     """.strip().split()
-    print(len(word1))
 
     word2 = """
     clam-toungued scurvy-ridden cross-eyed
     grog-abusing barnacle-backed yellow-bellied
     pus-faced twisted knuckle-dragging
     """.strip().split()
-    print(len(word2))
 
     word3 = """
     waster-of-skin cabin-boy peice-of-filth
     whale-fart cow-pie bag-of-vomit
     sack-o'maggots anchor-head swabber
     """.strip().split()
-    print(len(word3))
+
+    starters = ['Yer\' a', 
+                'That be true enough, but yer a', 
+                'You\'re a', 
+                'Har, Har! Methinks you\'re a', 
+                'Ha! You insult like a child. You\'re a',
+                'Is that the best you got? You\'re a',
+                'My fresh born lass swears better you, yer a',
+                'I have heard worse from\'a fish, you\'re a',
+                'Out of ideas? HA! Yer a']
+
+    tim = 'Cantankerous Tim says:'
+    ned = 'Twitchy Ned says:'
+
+    for i in range(rounds):
+        start = random.choice(starters)
+        w1 = random.choice(word1)
+        w2 = random.choice(word2)
+        w3 = random.choice(word3)
+        if int(i) % 2 == 0:
+            print('{} {} {}, {}, {}!!!\n'.format(tim, start, w1, w2, w3))
+            time.sleep(3)
+        else:
+            print('     {} {} {}, {}, {}!!!\n'.format(ned, start, w1, w2, w3))
+            time.sleep(3)
+
+    """future implementation make it go through
+       every possible combo without replication."""
+
+    if rounds > (9*9*9):
+        print('That\'s how you swear like a sailor')
 
 
 # --------------------------------------------------
